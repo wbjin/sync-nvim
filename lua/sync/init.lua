fc10173c7d040502e6e6debb59e832936a745b69
Wonbin Jin <wbjin@umich.edu>
2024-05-09 19:15:06 -0400

    Error checking

--- lua/sync/init.lua
+++ lua/sync/init.lua
@@ -1,13 +1,14 @@
 local M = {}
-local local_absolute_path = ""
-local dest_absolute_path = ""
-local dest_ssh = ""
+local local_absolute_path = nil
+local dest_absolute_path = nil
+local dest_ssh = nil
 
 M.setup = function()
     vim.api.nvim_create_user_command("SyncSetLocal",
         function(args)
             local path = vim.fn.input "path: "
             set_local(path)
+            print("Set local path")
         end,
         {}
     )
@@ -15,6 +16,7 @@ M.setup = function()
         function(args)
             local ssh = vim.fn.input "ssh: "
             set_ssh(ssh)
+            print("Set ssh destination")
         end,
         {}
     )
@@ -22,6 +24,7 @@ M.setup = function()
         function(args)
             local path = vim.fn.input "path: "
             set_dest(path)
+            print("Set destination path")
         end,
         {}
     )
@@ -33,6 +36,7 @@ M.setup = function()
             set_local(local_path)
             set_ssh(dest_ssh)
             set_dest(dest_path)
+            print("Set all")
         end,
         {}
     )
@@ -55,6 +59,10 @@ function set_ssh(ssh)
 end
 
 function sync()
+    if local_absolute_path == nil or dest_ssh == nil or dest_absolute_path == nil then
+        print("Please set configs")
+        return
+    end
     local command = "rsync -a " .. local_absolute_path .. " " .. dest_ssh .. ":" .. dest_absolute_path
     local ret = io.popen(command)
     local sucess = {ret:close()}
